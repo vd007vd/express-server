f1f299bc2a2245b9f99f69306a55a5c727bc1ba2
vd007vd <93114329+vd007vd@users.noreply.github.com>
2021-11-12 02:09:23 +0530

    Revert "Generic Routes Validation"

--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -1,17 +1,12 @@
 import {Router} from 'express';
 import TraineeController from './controller';
-import Validation from './validation';
-import validationHandler from './validationHandler'
 
 const traineeRouter = Router();
 
 traineeRouter.route('/')
-.get(validationHandler(Validation.get), TraineeController.get)
-.post(validationHandler(Validation.create), TraineeController.create)
-.put(validationHandler(Validation.update), TraineeController.update)
-.delete(validationHandler(Validation.delete), TraineeController.delete);
-
-traineeRouter.route('/:id')
-    .delete(validationHandler(Validation.delete), TraineeController.delete);
+.get(TraineeController.get)
+.post(TraineeController.create)
+.put(TraineeController.update)
+.delete(TraineeController.delete);
 
 export default traineeRouter;
