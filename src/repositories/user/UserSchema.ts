0d0c86343d7472be0b7801ed7d444760e9c208c1
Vipul <vipul.patil@dentsu.com>
2021-12-03 13:52:48 +0530

    testing

---
+++ src/repositories/user/UserSchema.ts
@@ -0,0 +1,16 @@
+import * as mongoose from 'mongoose';
+
+class UserSchema extends mongoose.Schema{
+    constructor(collections : any) {
+        const baseSchema = Object.assign({
+            _id: String,
+            name : String,
+            email: String,
+            role : String,
+            password : String,
+        });
+        super(baseSchema, collections);
+    }
+}
+
+export default UserSchema;
